d6008db6c5c6e6e5515736fd150f0c31a96b2319
ceYASH <yashthakurce@gmail.com>
2024-12-13 08:51:41 +0530

    datatypes in PHP

---
+++ datatypes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    class Test{
+
+    }
+    <?php
+    $a=150;//int
+    echo $a."<br>";
+
+    $b=23.45;//float
+    echo $b."<br>";
+
+    $c="Hello Yash";//string
+    echo $c."<br>";
+
+    $d=TRUE;//boolean
+    echo $d."<br>";
+
+    $ar=array("yash","naksh","shriyash");
+    print_r($ar);
+
+    $e=null;
+    echo $e;
+    // t=new Test();
+    ?>
+</body>
+</html>
