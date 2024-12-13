7153dbd951f15adb1fa52af33ceed7355d22d0a5
ceYASH <yashthakurce@gmail.com>
2024-12-13 08:31:42 +0530

    addition of two number

---
+++ variable.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>VARIABLE</title>
+</head>
+<body>
+    <?php
+    $a=100;
+    $b=50;
+    $c=$a+$b;
+    echo "Addition of ".$a." and " .$b. " is " .$c;
+    ?>
+</body>
+</html>
